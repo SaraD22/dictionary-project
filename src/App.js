4bc41f54ef5589f9f164b89700dc8a2337416230
SaraD22 <91880957+SaraD22@users.noreply.github.com>
2022-05-14 12:49:09 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          alt="logo"
-          // eslint-disable-next-line
-          className="App-logo img-fluid"
-        ></img>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            alt="logo"
+            // eslint-disable-next-line
+            className="App-logo img-fluid"
+          ></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Sara Duarte</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  // eslint-disable-next-line
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
